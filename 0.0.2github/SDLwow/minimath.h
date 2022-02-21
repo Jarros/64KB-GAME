8934a9bf735e11460a2c582769f5539c48012b04
Jaroslav <jarros97@gmail.com>
2022-02-21 19:53:37 +0300

    Delete 0.0.2github directory

--- 0.0.2github/SDLwow/minimath.h
+++
@@ -1,9 +0,0 @@
-////float sqrt(float x);
-//float sinm(float x);
-//float cosm(float x);
-////float atan(float x,float y);
-////float asin(float x);
-////float acos(float x);
-////float abss(float x);
-////float tan(float x);
-////float pow(float x, float y);
